eeb5e71eacca8840aedb18e3f71972caff1f57dd
labbbbbbbbb <724757025@qq.com>
2024-04-28 17:50:33 +0800

    加速度规划

--- CODE/Upper_trial01/UserCode/Upper/Upper_Servo/UpperServo.c
+++ CODE/Upper_trial01/UserCode/Upper/Upper_Servo/UpperServo.c
@@ -10,22 +10,18 @@ void Upper_Servo_Task(void *argument)
 {
     osDelay(1000);
     for (;;) {
-        // positionServo(current_angle[0], Core_xy.Motor_X);
-        // positionServo(current_angle[1], Core_xy.Motor_Y);
-        // positionServo(current_angle[2], Core_xy.Motor_Z);
-        float REF[3];
-        REF[0] = (Target.position.x) / BELT_LENGTH_PER_ROUND * 360.0f; // 所需要转的角度  单位：度
-        REF[1] = (Target.position.y) / BELT_LENGTH_PER_ROUND * 360.0f;
-        REF[2] = (Target.position.z) / BELT_LENGTH_PER_ROUND * 360.0f;
-        positionServo(REF[0], Core_xy.Motor_X);
-        positionServo(REF[1], Core_xy.Motor_Y);
-        positionServo(REF[2], Core_xy.Motor_Z);
+        positionServo(current_angle[0], Core_xy.Motor_X);
+        positionServo(current_angle[1], Core_xy.Motor_Y);
+        positionServo(current_angle[2], Core_xy.Motor_Z);
+        
         CanTransmit_DJI_1234(&hcan1,
                              Core_xy.Motor_X->speedPID.output,
                              Core_xy.Motor_Y->speedPID.output,
                              Core_xy.Motor_Z->speedPID.output,
                              0);
-             osDelay(10);
+        printf("%f,%f,%f,%f\n", Core_xy.Motor_X->AxisData.AxisVelocity, Core_xy.Motor_X->AxisData.AxisAngle_inDegree, Core_xy.Motor_X->speedPID.ref, Core_xy.Motor_X->speedPID.output);
+
+        osDelay(10);
 
     }
     
@@ -55,7 +51,7 @@ void Core_xy_Motor_init()               //电机初始化
     DJI_Init();
     for (int i = 0; i < 8; i++) {
         hDJI[i].speedPID.KP        = 5;
-        hDJI[i].speedPID.KI        = 2.0;
+        hDJI[i].speedPID.KI        = 1.0;
         hDJI[i].speedPID.KD        = 0.0;
         hDJI[i].speedPID.outputMax = 8000;
 
--- CODE/Upper_trial01/UserCode/Upper/Upper_StateMachine/StateMachine.c
+++ CODE/Upper_trial01/UserCode/Upper/Upper_StateMachine/StateMachine.c
@@ -14,30 +14,30 @@ float current_angle[3];
 
 void Core_xy_State_Task(void *argument)
 {
-    //osDelay(1000);
+    osDelay(1000);
 
      for (;;) {
-    //     float REF[3];
-    //     REF[0] = (Target.position.x) / BELT_LENGTH_PER_ROUND * 360.0f; // 所需要转的角度  单位：度
-    //     REF[1] = (Target.position.y) / BELT_LENGTH_PER_ROUND * 360.0f;
-    //     REF[2] = (Target.position.z) / BELT_LENGTH_PER_ROUND * 360.0f;
-    //     TickType_t StartTick = xTaskGetTickCount();
-    //     initial_angle[0] = Core_xy.Motor_X->AxisData.AxisAngle_inDegree;
-    //     initial_angle[1] = Core_xy.Motor_Y->AxisData.AxisAngle_inDegree;
-    //     initial_angle[2] = Core_xy.Motor_Z->AxisData.AxisAngle_inDegree;
+        float REF[3];
+        REF[0] = (Target.position.x) / BELT_LENGTH_PER_ROUND * 360.0f; // 所需要转的角度  单位：度
+        REF[1] = (Target.position.y) / BELT_LENGTH_PER_ROUND * 360.0f;
+        REF[2] = (Target.position.z) / BELT_LENGTH_PER_ROUND * 360.0f;
+        TickType_t StartTick = xTaskGetTickCount();
+        initial_angle[0] = Core_xy.Motor_X->AxisData.AxisAngle_inDegree;
+        initial_angle[1] = Core_xy.Motor_Y->AxisData.AxisAngle_inDegree;
+        initial_angle[2] = Core_xy.Motor_Z->AxisData.AxisAngle_inDegree;
 
-    //     _Bool isArray         = 0;
-    //     float diff[3]        = {0};
-    //     do {
-    //         TickType_t CurrentTick = xTaskGetTickCount();
-    //         float current_time     = (CurrentTick - StartTick) / 1000.0;
-    //         VelocityPlanning(initial_angle[0], 1000, 100, REF[0], current_time, &(current_angle[0]));
-    //         VelocityPlanning(initial_angle[1], 1000, 100, REF[1], current_time, &(current_angle[1]));
-    //         VelocityPlanning(initial_angle[2], 1000, 100, REF[2], current_time, &(current_angle[2]));
-    //         for (uint16_t i = 0; i < 3; i++) { diff[i] = fabs(REF[i] - current_angle[i]); }
-    //         if ((diff[0] < 0.1) && (diff[1] < 0.1) && (diff[2] < 1)) { isArray = 1; }
+        _Bool isArray         = 0;
+        float diff[3]        = {0};
+        do {
+            TickType_t CurrentTick = xTaskGetTickCount();
+            float current_time     = (CurrentTick - StartTick) / 1000.0;
+            VelocityPlanning(initial_angle[0], 2000, 100, REF[0], current_time, &(current_angle[0]));
+            VelocityPlanning(initial_angle[1], 1000, 100, REF[1], current_time, &(current_angle[1]));
+            VelocityPlanning(initial_angle[2], 1000, 100, REF[2], current_time, &(current_angle[2]));
+            for (uint16_t i = 0; i < 3; i++) { diff[i] = fabs(REF[i] - current_angle[i]); }
+            if ((diff[0] < 0.1) && (diff[1] < 0.1) && (diff[2] < 1)) { isArray = 1; }
 
-    // } while (!isArray);
+    } while (!isArray);
 
         osDelay(10);
     }
